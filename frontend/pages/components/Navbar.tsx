3178cb36d90019fd3249cd8826f5c8e924c6a4bc
cagrigit-hub <cagrikutayok@hotmail.com>
2023-12-09 17:14:30 +0300

    push

--- frontend/pages/components/Navbar.tsx
+++ frontend/pages/components/Navbar.tsx
@@ -48,11 +48,11 @@ function Navbar() {
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>Item 1</a>
+            <a>*</a>
           </li>
           <li>
             <details>
-              <summary>Parent</summary>
+              <summary>*</summary>
               <ul className="p-2">
                 <li>
                   <a>Submenu 1</a>
@@ -64,7 +64,7 @@ function Navbar() {
             </details>
           </li>
           <li>
-            <a>Item 3</a>
+            <a>*</a>
           </li>
         </ul>
       </div>
