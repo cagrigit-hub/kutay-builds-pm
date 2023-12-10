03e909a3bb4bf1c05f2a74d815b3d407baf64bb4
cagrigit-hub <cagrikutayok@hotmail.com>
2023-12-10 03:53:38 +0300

    local storage + cookies etc. is understood

--- google-ext/index.js
+++ google-ext/index.js
@@ -1,1 +1,19 @@
-// get cookie
+// store 12345 in local storage
+localStorage.setItem("myCat", "Tom");
+
+const button = document.getElementById("get_token");
+
+function retrieveToken() {
+  var token = localStorage.getItem("myCat");
+  console.log(token);
+  return token;
+}
+
+button.addEventListener("click", retrieveToken);
+
+const set_token = document.getElementById("set_token");
+function setToken() {
+  localStorage.setItem("myCat", "Tom");
+}
+
+set_token.addEventListener("click", setToken);
